cb85bff0e66ec86f3a8a3de4bb6613857253017f
jisantamariar1 <jisantamariar1@uce.edu.ec>
2026-01-15 19:44:19 -0500

    CRUD de API Quarkus

---
+++ src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
@@ -0,0 +1,21 @@
+package uce.edu.web.api.matricula.interfaces;
+
+import java.util.List;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import uce.edu.web.api.matricula.application.EstudianteService;
+import uce.edu.web.api.matricula.domain.Estudiante;
+
+@Path("/estudiantes")
+public class EstudianteResource {
+    @Inject
+    private EstudianteService estudianteService;
+
+    @GET
+    @Path("/todos")
+    public List<Estudiante> listarTodos(){
+        return this.estudianteService.listarTodos();
+    }
+}
