76a6830f8a736dc079dc6d19a7f0a819a1cfa82c
jisantamariar1 <jisantamariar1@uce.edu.ec>
2026-02-04 19:51:06 -0500

    varios roles

--- src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
@@ -40,7 +40,7 @@ public class EstudianteResource {
     @GET
     @Path("")
     @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed("admin")
+    @RolesAllowed({"admin", "user", "docente"})
     public List<EstudianteRepresentation> listarTodos() {
         System.out.println("XXXXXXXXXXX  Listar todos los estudiantes XXXXXXXXXXX ");
         List<EstudianteRepresentation> lista = new ArrayList<>();
